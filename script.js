24b74b8dafc3063e15b6d06683be8936e390553b
pmgutierrez <pmgutierrez2700@gmail.com>
2024-03-15 11:21:06 -0300

    Se grega arrays y funcion btnEncriptar() a script.js y se chequea funcionamiento  console.log

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert("Hola Mundo");
+const campo_texto = document.querySelector("#campo-texto");
+const campo_mensaje = document.querySelector("#campo-mensaje");
+
+//console.log(campo_texto, campo_mensaje);
+
+const matriz_code = [
+    ["e", "enter"], // indice 0
+    ["i", "imes"],  // indice 1
+    ["a", "ai"],    // indice 2
+    ["o", "ober"],  // indice 3
+    ["u", "ufat"],  // indice 4
+];
+
+function btnEncriptar(){
+    const texto = campo_texto.value;
+    console.log(texto);
+}
+
